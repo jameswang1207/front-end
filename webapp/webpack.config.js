80263fd3cce3e4a14ed632a0a183740395b91caa
jameswang <jameswang@augmentum.com.cn>
2016-06-08 17:58:08 +0800

    vue demo and webpack

---
+++ webapp/webpack.config.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: './static/',
+    publicPath: '/static/',
+    filename: 'build.js'
+  },
+  module: {
+    noParse: /es6-promise\.js$/,
+    loaders: [
+      {test: /\.vue$/,loader: 'vue'},
+      {test: /\.js$/,exclude: /node_modules|vue\/dist|vue-router\/|vue-loader\/|vue-hot-reload-api\//,loader: 'babel'},
+    ]
+  },
+  babel: {
+    presets: ['es2015'],
+    plugins: ['transform-runtime']
+  },
+  devtool: 'source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins = [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin()
+  ]
+} else {
+  module.exports.devtool = '#source-map'
+}
