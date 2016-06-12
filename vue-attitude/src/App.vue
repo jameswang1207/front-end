c68e47ccef4531881346793144bb82bb83f4c76d
jameswang <jameswang@augmentum.com.cn>
2016-06-12 18:24:13 +0800

    add vue example

--- vue-attitude/src/App.vue
+++ vue-attitude/src/App.vue
@@ -3,7 +3,7 @@
     <img class="logo" src="./assets/logo.png">
     <hello></hello>
     <p>
-      Welcome to your Vue.js app!
+      Welcome to your Vue.js app james !
     </p>
     <p>
       To get a better understanding of how this boilerplate works, check out
